d577a2da6165caf110018d68ef2c3225345d4bbb
Michael Amie <michael.amie@gmail.com>
2012-08-23 13:52:11 -0700

    Moved main body into if __name__ == '__main__': conditional.

--- ca-conway.py
+++ ca-conway.py
@@ -20,69 +20,68 @@ def countNeighbors(map, numRows, numCols, row, col):
   return count
 
 
-################################################################################
+if __name__ == '__main__':
+  # Set up curses
+  try:
+    screen = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+    curses.curs_set(0)
+    screen.nodelay(True)
 
-# Set up curses
-try:
-  screen = curses.initscr()
-  curses.noecho()
-  curses.cbreak()
-  curses.curs_set(0)
-  screen.nodelay(True)
+    # Values
+    rows = range(23)
+    cols = range(80)
+    delay = .1
 
-  # Values
-  rows = range(23)
-  cols = range(80)
-  delay = .1
+    # Create empty map
+    map = [[False for col in cols] for row in rows]
 
-  # Create empty map
-  map = [[False for col in cols] for row in rows]
+    # Populate map with a floater
+    map[10][10] = 1;
+    map[10][11] = 1;
+    map[10][9] = 1;
+    map[9][11] = 1;
+    map[8][10] = 1;
 
-  # Populate map with a floater
-  map[10][10] = 1;
-  map[10][11] = 1;
-  map[10][9] = 1;
-  map[9][11] = 1;
-  map[8][10] = 1;
-
-  # Draw map before iterations
-  drawMap(screen, map, rows, cols)
-  screen.refresh()
-  time.sleep(delay)
-
-  # Iterate cellular automata algorithm
-  iteration = 0
-  key = 0
-  while key != 27:
-    # Check if ESC has been hit
-    key = screen.getch()
-
-    # Initialize next map
-    nextMap = [[False for col in cols] for row in rows]
-
-    # Count the neighbors of each cell and populate next map
-    for row in rows:
-      for col in cols:
-        count = countNeighbors(map, len(rows), len(cols), row, col)
-        newState = False
-        if map[row][col] == True and (count == 2 or count == 3):
-          newState = True
-        if map[row][col] == False and count == 3:
-          newState = True
-        nextMap[row][col] = newState
-
-    # Update map
-    map = nextMap
-
-    # Draw map for each iteration
+    # Draw map before iterations
     drawMap(screen, map, rows, cols)
     screen.refresh()
     time.sleep(delay)
 
-  screen.getch()
+    # Iterate cellular automata algorithm
+    iteration = 0
+    key = 0
+    while key != 27:
+      # Check if ESC has been hit
+      key = screen.getch()
 
-# Restore terminal to regular state
-finally:
-  curses.echo()
-  curses.nocbreak()
-  curses.endwin()
+      # Initialize next map
+      nextMap = [[False for col in cols] for row in rows]
+
+      # Count the neighbors of each cell and populate next map
+      for row in rows:
+        for col in cols:
+          count = countNeighbors(map, len(rows), len(cols), row, col)
+          newState = False
+          if map[row][col] == True and (count == 2 or count == 3):
+            newState = True
+          if map[row][col] == False and count == 3:
+            newState = True
+          nextMap[row][col] = newState
+
+      # Update map
+      map = nextMap
+
+      # Draw map for each iteration
+      drawMap(screen, map, rows, cols)
+      screen.refresh()
+      time.sleep(delay)
+
+    screen.getch()
+
+  # Restore terminal to regular state
+  finally:
+    curses.echo()
+    curses.nocbreak()
+    curses.endwin()

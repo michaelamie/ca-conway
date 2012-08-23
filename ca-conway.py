0643d7396eca91918e8aa56e2f0528134f552b47
Michael Amie <michael.amie@gmail.com>
2012-08-22 20:17:28 -0700

    Fixed next generation rules so that ca-conway works properly with a "floater" now and updated countNeighbors() function.

--- ca-conway.py
+++ ca-conway.py
@@ -11,26 +11,14 @@ def drawMap(screen, map, rows, cols):
 
 def countNeighbors(map, numRows, numCols, row, col):
   count = 0
-  # Left column
-  if row-1 >= 0 and col-1 >= 0 and map[row-1][col-1] == True:
-    count += 1
-  if col-1 >= 0 and map[row][col-1] == True:
-    count += 1
-  if row+1 < numRows and col-1 >= 0 and map[row+1][col-1] == True:
-    count += 1
-  # Center column
-  if row-1 >= 0 and map[row-1][col] == True:
-    count += 1
-  if row+1 < numRows and map[row+1][col] == True:
-    count += 1
-  # Right column
-  if row-1 >= 0 and col+1 < numCols and map[row-1][col+1] == True:
-    count += 1
-  if col+1 < numCols and map[row][col+1] == True:
-    count += 1
-  if row+1 < numRows and col+1 < numCols and map[row+1][col+1] == True:
-    count += 1
+  for y in [(row-1) % numRows, row, (row+1) % numRows]:
+    for x in [(col-1) % numCols, col, (col+1) % numCols]:
+      if map[y][x]:
+          count += 1
+  if map[row][col]:
+    count -= 1
   return count
+
 
 ################################################################################
 
@@ -43,50 +31,53 @@ try:
   screen.nodelay(True)
 
   # Values
-  rows = range(20)
-  cols = range(38)
+  rows = range(23)
+  cols = range(80)
+  delay = .1
 
-  # Populate and randomize map
+  # Create empty map
   map = [[False for col in cols] for row in rows]
-  for row in rows[1:len(rows)/4]:
-    for col in cols[1:len(cols)/4]:
-      map[row][col] = random.randint(1, 100) < 36
+
+  # Populate map with a floater
+  map[10][10] = 1;
+  map[10][11] = 1;
+  map[10][9] = 1;
+  map[9][11] = 1;
+  map[8][10] = 1;
 
   # Draw map before iterations
   drawMap(screen, map, rows, cols)
   screen.refresh()
-  time.sleep(.1)
+  time.sleep(delay)
 
   # Iterate cellular automata algorithm
   iteration = 0
   key = 0
   while key != 27:
-    key = screen.getch()
     # Check if ESC has been hit
-    changed = False
+    key = screen.getch()
+
+    # Initialize next map
+    nextMap = [[False for col in cols] for row in rows]
+
+    # Count the neighbors of each cell and populate next map
     for row in rows:
       for col in cols:
         count = countNeighbors(map, len(rows), len(cols), row, col)
-        # Modify the current cell
-        if map[row][col] == True and count < 2:
-          map[row][col] = False
-          changed = True
-        elif map[row][col] == True and (count > 3):
-          map[row][col] = False
-          changed = True
-        elif map[row][col] == False and count == 3:
-          map[row][col] = True
-          changed = True
+        newState = False
+        if map[row][col] == True and (count == 2 or count == 3):
+          newState = True
+        if map[row][col] == False and count == 3:
+          newState = True
+        nextMap[row][col] = newState
+
+    # Update map
+    map = nextMap
 
     # Draw map for each iteration
     drawMap(screen, map, rows, cols)
     screen.refresh()
-    time.sleep(.1)
-
-    # Break out of the loop if we've reached equilibrium
-    if not changed:
-      screen.nodelay(False)
-      break
+    time.sleep(delay)
 
   screen.getch()
 

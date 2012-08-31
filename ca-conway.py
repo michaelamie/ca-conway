7707324c0422b82b3207bb8c0560fa576edeb94e
Michael Amie <michael.amie@gmail.com>
2012-08-30 22:04:33 -0700

    enamed map variable to avoid identifier overlap with map()

--- ca-conway.py
+++ ca-conway.py
@@ -2,20 +2,20 @@
 import curses, random, time
 
 
-def drawMap(screen, map, rows, cols):
+def drawGrid(screen, grid, rows, cols):
   screen.clear()
   for row in rows:
     for col in cols:
-      if map[row][col] == True:
+      if grid[row][col] == True:
         screen.addch(row, col, '*')
 
-def countNeighbors(map, numRows, numCols, row, col):
+def countNeighbors(grid, numRows, numCols, row, col):
   count = 0
   for y in [(row-1) % numRows, row, (row+1) % numRows]:
     for x in [(col-1) % numCols, col, (col+1) % numCols]:
-      if map[y][x]:
+      if grid[y][x]:
           count += 1
-  if map[row][col]:
+  if grid[row][col]:
     count -= 1
   return count
 
@@ -34,18 +34,18 @@ if __name__ == '__main__':
     cols = range(80)
     delay = .1
 
-    # Create empty map
-    map = [[False for col in cols] for row in rows]
+    # Create empty grid
+    grid = [[False for col in cols] for row in rows]
 
-    # Populate map with a floater
-    map[10][10] = 1;
-    map[10][11] = 1;
-    map[10][9] = 1;
-    map[9][11] = 1;
-    map[8][10] = 1;
+    # Populate grid with a floater
+    grid[10][10] = 1;
+    grid[10][11] = 1;
+    grid[10][9] = 1;
+    grid[9][11] = 1;
+    grid[8][10] = 1;
 
-    # Draw map before iterations
-    drawMap(screen, map, rows, cols)
+    # Draw grid before iterations
+    drawGrid(screen, grid, rows, cols)
     screen.refresh()
     time.sleep(delay)
 
@@ -56,25 +56,25 @@ if __name__ == '__main__':
       # Check if ESC has been hit
       key = screen.getch()
 
-      # Initialize next map
-      nextMap = [[False for col in cols] for row in rows]
+      # Initialize next grid
+      nextGrid = [[False for col in cols] for row in rows]
 
-      # Count the neighbors of each cell and populate next map
+      # Count the neighbors of each cell and populate next grid
       for row in rows:
         for col in cols:
-          count = countNeighbors(map, len(rows), len(cols), row, col)
+          count = countNeighbors(grid, len(rows), len(cols), row, col)
           newState = False
-          if map[row][col] == True and (count == 2 or count == 3):
+          if grid[row][col] == True and (count == 2 or count == 3):
             newState = True
-          if map[row][col] == False and count == 3:
+          if grid[row][col] == False and count == 3:
             newState = True
-          nextMap[row][col] = newState
+          nextGrid[row][col] = newState
 
-      # Update map
-      map = nextMap
+      # Update grid
+      grid = nextGrid
 
-      # Draw map for each iteration
-      drawMap(screen, map, rows, cols)
+      # Draw grid for each iteration
+      drawGrid(screen, grid, rows, cols)
       screen.refresh()
       time.sleep(delay)
 
